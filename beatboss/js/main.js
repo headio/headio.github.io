b99a37771105ec2b92b5b4ca3ad1333617dc36dd
Hitao <taoabc@gmail.com>
2015-02-09 21:14:33 +0800

    use origin source

--- beatboss/js/main.js
+++ beatboss/js/main.js
@@ -1,1 +1,296 @@
-function echo(c,b){var a="你好";alert("hello world")}var sound=(function(){var d={};var a;var c=false;var b=[];d.SetBGM=function(e){a=ult.audio.CreateAudio(e,{preload:true,loop:true,autoplay:true});c=a.autoplay};d.PlayBGM=function(){if(a&&!c){a.play();c=true}};d.PauseBGM=function(){if(a&&c){a.pause();c=false}};d.AddBeatenSound=function(e){b.push(ult.audio.CreateAudio(e))};d.RandomPlayBeatenSound=function(){var e=b[ult.GetRandom(0,b.length)];e.play()};return d})();var page1=(function(){var e={};var a=false;var b=false;var c=[".boss",".beat-effect",".beat-button"];var d;e.Start=function(){$("#beat-button").click(function(){if(page1.AniPlaying()){page1.AniStop()}else{page1.AniStart();sound.PlayBGM()}$("#popup-rule").show()});$("#beat-title").on("webkitAnimationEnd",function(){$(this).removeClass("normal");$(this).addClass("beaten")});$("#popup-rule-confirm").click(function(){$("#popup-rule").hide();e.PageEnd();page2.Start()});sound.SetBGM("res/bgm.mp3");e.AniStart()};e.AniStart=function(){if(!a){d=setInterval(function(){if(b){SwitchToNormalStatus()}else{SwitchToBeatenStatus()}},200);a=true}};e.AniPlaying=function(){return a};e.AniStop=function(){if(a){clearInterval(d);if(b){SwitchToNormalStatus()}a=false}};e.PageEnd=function(){$("#startpage").hide();e.AniStop();sound.PauseBGM()};SwitchToNormalStatus=function(){for(var f in c){$(c[f]).removeClass("beaten");$(c[f]).addClass("normal")}b=false};SwitchToBeatenStatus=function(){for(var f in c){$(c[f]).removeClass("normal");$(c[f]).addClass("beaten")}b=true};Reset=function(){a=false;b=false;clearInterval(d)};return e})();var page2=(function(){var h={};var g=5;var f=g;var i=0;var d=0;var b=0;var c=true;var e=[{min_count:0,max_count:10,boss_head:"boss-head-1",boss_body:"boss-body-1"},{min_count:11,max_count:20,boss_head:"boss-head-2",boss_body:"boss-body-2"},{min_count:21,max_count:30,boss_head:"boss-head-3",boss_body:"boss-body-3"},{min_count:31,max_count:40,boss_head:"boss-head-4",boss_body:"boss-body-3"},{min_count:41,max_count:50,boss_head:"boss-head-5",boss_body:"boss-body-4"},{min_count:51,max_count:60,boss_head:"boss-head-6",boss_body:"boss-body-4"},{min_count:61,max_count:999,boss_head:"boss-head-7",boss_body:"boss-body-5"}];var a=0;h.Start=function(){Reset();$("#restart-button").click(function(){Reset()});$(".beaten-area").click(function(){if(c){i=setInterval(function(){--f;SetLeftTime(f);if(f==0){clearInterval(i);SetLeftTime(0);SetPopupShareCount(b);$("#popup-share").show()}},1000);c=false}sound.RandomPlayBeatenSound();if(d==0){RightBeatBoss()}else{if(d==1){LeftBeatBoss()}}SetScore(b);SetBossWithStage()});$("#popup-share-confirm").click(function(){if(ult.PhoneInputCorrect($("#phone-number").val())){alert("开始分享");$("#popup-share").hide();Reset()}else{alert("手机号错误")}});$("#beatpage").show();sound.AddBeatenSound("res/beat1.mp3");sound.AddBeatenSound("res/beat2.mp3")};SetScore=function(j){$("#beat-score").html(String(j))};SetLeftTime=function(j){$("#left-time").html(String(j))};GetScore=function(){return Number($("#beat-score").html())};GetLeftTime=function(){return Number($("#left-time").html())};SetBossWithStage=function(){var m=e.length;var l={};var j=false;for(var k in e){if(b>=e[k].min_count&&b<=e[k].max_count){l=e[k];j=true;break}}if(j){$("#boss-body").attr("class","boss-body");$("#boss-head").attr("class","boss-head");$("#boss-body").addClass(l.boss_body);$("#boss-head").addClass(l.boss_head)}};ResetBoss=function(){$("#boss-body").attr("class","boss-body boss-body-1");$("#boss-head").attr("class","boss-head boss-head-1")};LeftBeatBoss=function(){if(d==1){++b;$("#boss-head").hide();$("#boss-beaten-right").hide();$("#boss-beaten-left").show();clearTimeout(a);a=setTimeout(function(){$("#boss-beaten-left").hide();$("#boss-head").show()},400);d=0}};RightBeatBoss=function(){if(d==0){++b;$("#boss-head").hide();$("#boss-beaten-left").hide();$("#boss-beaten-right").show();clearTimeout(a);a=setTimeout(function(){$("#boss-beaten-right").hide();$("#boss-head").show()},400);d=1}};SetPopupShareCount=function(j){$("#share-count").html(String(j))};Reset=function(){clearInterval(i);f=g;i=0;d=0;b=0;c=true;SetLeftTime(f);d=ult.GetRandom(0,2);SetScore(b)};return h})();$(document).ready(function(){page1.Start()});
+var sound = (function(){
+    var e_ = { };
+    
+    var bgm;
+    var bgmplaying = false;
+    var beaten_sounds = [];
+    
+    e_.SetBGM = function(path) {
+        bgm = ult.audio.CreateAudio(path, {preload: true, loop: true, autoplay: true});
+        bgmplaying = bgm.autoplay;
+    };
+    
+    e_.PlayBGM = function() {
+        if (bgm && !bgmplaying) {
+            bgm.play();
+            bgmplaying = true;
+        }
+    };
+    
+    e_.PauseBGM = function() {
+        if (bgm && bgmplaying) {
+            bgm.pause();
+            bgmplaying = false;
+        }
+    };
+    
+    e_.AddBeatenSound = function(source) {
+        beaten_sounds.push(ult.audio.CreateAudio(source));
+    };
+    
+    e_.RandomPlayBeatenSound = function() {
+        var randSound = beaten_sounds[ult.GetRandom(0, beaten_sounds.length)];
+        randSound.play();
+    };
+    
+    return e_;
+})();
+
+var page1 = (function(){
+    var e_ = { };
+    
+    var ani_playing = false;
+    var beaten_status = false;
+    var ani_class = [".boss", ".beat-effect", ".beat-button"];
+    var ani_interval;
+    
+    e_.Start = function() {
+        $("#beat-button").click(function() {
+            if (page1.AniPlaying()) {
+                page1.AniStop();
+                //sound.PauseBGM();
+            } else {
+                page1.AniStart();
+                sound.PlayBGM();
+            }
+            $("#popup-rule").show();
+        });
+        
+        $("#beat-title").on("webkitAnimationEnd", function() {
+            $(this).removeClass("normal");
+            $(this).addClass("beaten");
+        });
+        
+        $("#popup-rule-confirm").click(function() {
+            $("#popup-rule").hide();
+            e_.PageEnd();
+            page2.Start();
+        });
+        
+        sound.SetBGM("res/bgm.mp3");
+        e_.AniStart();
+    };
+    
+    e_.AniStart = function() {
+        if (!ani_playing) {
+            ani_interval = setInterval(function() {
+                if (beaten_status) {
+                    SwitchToNormalStatus();
+                } else {
+                    SwitchToBeatenStatus();
+                }
+            }, 200);
+            ani_playing = true;
+        }
+    };
+    
+    e_.AniPlaying = function() {
+        return ani_playing;
+    };
+    
+    e_.AniStop = function() {
+        if (ani_playing) {
+            clearInterval(ani_interval);
+            if (beaten_status) {
+                SwitchToNormalStatus();
+            }
+            ani_playing = false;
+        }
+    };
+    
+    e_.PageEnd = function() {
+        $("#startpage").hide();
+        e_.AniStop();
+        sound.PauseBGM();
+    };
+    
+    SwitchToNormalStatus = function() {
+        for (var ele in ani_class) {
+            $(ani_class[ele]).removeClass("beaten");
+            $(ani_class[ele]).addClass("normal");
+        }
+        beaten_status = false;
+    };
+    
+    SwitchToBeatenStatus = function() {
+        for (var ele in ani_class) {
+            $(ani_class[ele]).removeClass("normal");
+            $(ani_class[ele]).addClass("beaten");
+        }
+        beaten_status = true;
+    };
+    
+    Reset = function() {
+        ani_playing = false;
+        beaten_status = false;
+        clearInterval(ani_interval);
+    };
+    
+    return e_;
+})();
+
+var page2 = (function() {
+    var e_ = { };
+    
+    var time_out = 5;
+    var left_time = time_out;
+    var game_timer = 0;
+    var last_hand = 0; // 0 for left, 1 for right
+    var beat_count = 0;
+    var first_beat = true;
+    var beat_stage = [
+        {min_count: 0, max_count: 10, boss_head: "boss-head-1", boss_body: "boss-body-1"},
+        {min_count: 11, max_count: 20, boss_head: "boss-head-2", boss_body: "boss-body-2"},
+        {min_count: 21, max_count: 30, boss_head: "boss-head-3", boss_body: "boss-body-3"},
+        {min_count: 31, max_count: 40, boss_head: "boss-head-4", boss_body: "boss-body-3"},
+        {min_count: 41, max_count: 50, boss_head: "boss-head-5", boss_body: "boss-body-4"},
+        {min_count: 51, max_count: 60, boss_head: "boss-head-6", boss_body: "boss-body-4"},
+        {min_count: 61, max_count: 999, boss_head: "boss-head-7", boss_body: "boss-body-5"}
+    ];
+    var beat_timeout = 0;
+    
+    e_.Start = function() {
+        Reset();
+        
+        $("#restart-button").click(function() {
+            Reset();
+        });
+        
+        $(".beaten-area").click(function() {
+            if (first_beat) {
+                game_timer = setInterval(function() {
+                    --left_time;
+                    SetLeftTime(left_time);
+                    if (left_time == 0) {
+                        clearInterval(game_timer);
+                        SetLeftTime(0);
+                        SetPopupShareCount(beat_count);
+                        $("#popup-share").show();
+                    }
+                }, 1000);
+                
+                first_beat = false;
+            }
+            
+            sound.RandomPlayBeatenSound();
+            if (last_hand == 0) {
+                RightBeatBoss();
+            } else if (last_hand == 1) {
+                LeftBeatBoss();
+            }
+            SetScore(beat_count);
+            SetBossWithStage();
+        });
+        
+        $("#popup-share-confirm").click(function() {
+            if (ult.PhoneInputCorrect($("#phone-number").val())) {
+                alert("开始分享");
+                $("#popup-share").hide();
+                Reset();
+            } else {
+                alert("手机号错误");
+            }
+        });
+        
+        $("#beatpage").show();
+        
+        sound.AddBeatenSound("res/beat1.mp3");
+        sound.AddBeatenSound("res/beat2.mp3");
+    };
+    
+    
+    SetScore = function(score) {
+        $("#beat-score").html(String(score));
+    };
+    
+    SetLeftTime = function(left_time) {
+        $("#left-time").html(String(left_time));
+    };
+    
+    GetScore = function() {
+        return Number($("#beat-score").html());
+    };
+    
+    GetLeftTime = function() {
+        return Number($("#left-time").html());
+    };
+    
+    SetBossWithStage = function() {
+        var stage_count = beat_stage.length;
+        var stage_picked = { };
+        var stage_getted = false;
+        for (var i in beat_stage) {
+            if (beat_count >= beat_stage[i].min_count && beat_count <= beat_stage[i].max_count) {
+                stage_picked = beat_stage[i];
+                stage_getted = true;
+                break;
+            }
+        }
+        if (stage_getted) {
+            $("#boss-body").attr("class", "boss-body");
+            $("#boss-head").attr("class", "boss-head");
+            $("#boss-body").addClass(stage_picked.boss_body);
+            $("#boss-head").addClass(stage_picked.boss_head);
+        }
+    };
+    
+    ResetBoss = function() {
+        $("#boss-body").attr("class", "boss-body boss-body-1");
+        $("#boss-head").attr("class", "boss-head boss-head-1");
+    };
+    
+    LeftBeatBoss = function() {
+        if (last_hand == 1) {
+            ++beat_count;
+            $("#boss-head").hide();
+            $("#boss-beaten-right").hide();
+            $("#boss-beaten-left").show();
+            clearTimeout(beat_timeout);
+            beat_timeout = setTimeout(function() {
+                $("#boss-beaten-left").hide();
+                $("#boss-head").show();
+            }, 400);
+            
+            last_hand = 0;
+        }
+    };
+    
+    RightBeatBoss = function() {
+        if (last_hand == 0) {
+            ++beat_count;
+            $("#boss-head").hide();
+            $("#boss-beaten-left").hide();
+            $("#boss-beaten-right").show();
+            clearTimeout(beat_timeout);
+            beat_timeout = setTimeout(function() {
+                $("#boss-beaten-right").hide();
+                $("#boss-head").show();
+            }, 400);
+            
+            last_hand = 1;
+        }
+    };
+    
+    SetPopupShareCount = function(count) {
+        $("#share-count").html(String(count));
+    };
+    
+    Reset = function() {
+        clearInterval(game_timer);
+        left_time = time_out;
+        game_timer = 0;
+        last_hand = 0; // 0 for left, 1 for right
+        beat_count = 0;
+        first_beat = true;
+        SetLeftTime(left_time);
+        last_hand = ult.GetRandom(0, 2);
+        SetScore(beat_count);
+    };
+    
+    return e_;
+})();
+
+$(document).ready(function() {
+    page1.Start();
+    //page2.Start();
+});
--- beatboss/js/ult.js
+++ beatboss/js/ult.js
@@ -1,1 +1,30 @@
-var ult=(function(){var a={};a.IsIos=function(){return/ipad|iphone|ipod/g.test(navigator.userAgent.toLowerCase())};a.PhoneInputCorrect=function(c){var b=/^1[3,4,5,8]\d{9}$/;return b.test(c)};a.GetRandom=function(c,b){return Math.floor(Math.random()*(b-c)+c)};return a})();ult.audio=(function(){var b={},a={};b.CreateAudio=function(c,d){return $.extend(document.createElement("audio"),{src:c},d)};return b})();
+var ult = (function() {
+    var e_ = { };
+    
+    e_.IsIos = function() {
+        return /ipad|iphone|ipod/g.test(navigator.userAgent.toLowerCase());
+    };
+    
+    e_.PhoneInputCorrect = function(phone_number) {
+        var patern = /^1[3,4,5,8]\d{9}$/;
+        return patern.test(phone_number);
+    };
+    
+    // get random number from [begin, end)
+    e_.GetRandom = function(begin_number, end_number) {
+        return Math.floor(Math.random() * (end_number - begin_number) + begin_number)
+    };
+    
+    return e_;
+})();
+
+ult.audio = (function(){
+    var e_ = { }, p_ = { };
+    
+    e_.CreateAudio = function(path, param) {
+        return $.extend(document.createElement("audio"),
+                        { src: path }, param);
+    };
+    
+    return e_;
+})();
